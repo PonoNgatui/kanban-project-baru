91eaedd61f238c9e03c490ab5321924652727755
PonoNgatui <ponongatui8@gmail.com>
2023-07-21 20:04:06 +0700

    Apply common layout

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -58,7 +58,8 @@ class TaskController extends Controller
 
     public function index()
     {
+        $pageTitle = 'Task List'; //ditambahkan
         $tasks = $this->tasks;
-        return view('tasks.index', ['tasks' => $tasks]);
+        return view('tasks.index', ['pageTitle' => $pageTitle,'tasks' => $tasks]);
     }
 }
