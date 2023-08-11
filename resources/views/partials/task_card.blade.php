395e3aff2d26c5bbec8ab201a6dec4836efa32f7
PonoNgatui <ponongatui8@gmail.com>
2023-08-12 04:22:09 +0700

    Set Task Authorization

--- resources/views/partials/task_card.blade.php
+++ resources/views/partials/task_card.blade.php
@@ -1,5 +1,6 @@
 <div class="task-progress-card">
     <div class="task-progress-card-left">
+      @can('complete', $task)
       @if ($task->status == 'completed')
         <div class="material-icons task-progress-card-top-checked">check_circle</div>
       @else
@@ -11,8 +12,11 @@
           <button class="material-icons task-progress-card-top-check">check_circle</button>
         </form>
       @endif
+      @endcan
+
+      @can('update', $task)
       <a href="{{ route('tasks.edit', ['id' => $task->id]) }}" class="material-icons task-progress-card-top-edit">more_vert</a>
-      
+      @endcan
     </div>
     <p class="task-progress-card-title">{{ $task->name }}</p>
     <div>
@@ -25,6 +29,7 @@
       <p>Owner: <strong>{{ $task->user->name }}</strong></p>
     </div>
     <div class="@if ($leftStatus) task-progress-card-left @else task-progress-card-right @endif">
+      @can('move',$task)
       @if ($leftStatus)
       <form
         action="{{ route('tasks.move', ['id' => $task->id, 'status' => $leftStatus]) }}" 
@@ -46,6 +51,7 @@
       <button class="material-icons">chevron_right</button>
     </form>
       @endif
+      @endcan
       
     </div>
   </div>
